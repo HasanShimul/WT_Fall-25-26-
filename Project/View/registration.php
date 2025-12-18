4f38efe99e7f0275541327d33ed34df690605792
=ShimulHasan <=ss.shimulhasan00@gmail.com>
2025-12-18 08:53:29 +0600

    Project: registration start

---
+++ Project/View/registration.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+       <div>
+       <h1>Doctor Registratioin Panel</h1>
+        <div>
+            <div>
+                <form action="" method="POST">
+                    <div>
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name">
+                    </div>
+                    <button type="submit">Registration</button>
+                </form>
+            </div>
+        </div>
+       </div>
+    </body>
+</html>
