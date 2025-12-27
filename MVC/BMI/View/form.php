bb0bbfeeb65458a30f2a38e4f5e833828f80f076
=ShimulHasan <=ss.shimulhasan00@gmail.com>
2025-12-27 23:44:59 +0600

    day night using js

--- MVC/BMI/View/form.php
+++ MVC/BMI/View/form.php
@@ -3,9 +3,16 @@
 <head>
     <title>BMI Form</title>
 </head>
-<body>
+<style>
+    .night{
+        color:white;
+        background-color:black;
+    }
+</style>
+<body id="body">
     <center>
     <h2>Enter Your Details</h2>
+    <button id="mode">Day/Night</button>
     <form method="POST" action="">
         Name: <input type="text" name="name"  ><br><br>
         Height (cm): <input type="number" name="height"  ><br><br>
@@ -14,4 +21,12 @@
     </form>
     </center>
 </body>
+<script>
+      const btn =  document.getElementById("mode");
+       
+    btn.addEventListener('click',()=>{
+      body.classList.toggle("night");
+       
+    });
+</script>
 </html>
--- MVC/BMI/View/result.php
+++ MVC/BMI/View/result.php
@@ -3,8 +3,15 @@
 <head>
     <title>BMI Result</title>
 </head>
+<style>
+    .night-mode{
+        color:yellow;
+        background-color:rgb(100,100,100);
+    }
+</style>
 <body>
     <center>
+        <button id="mode">Day/Night</button>
     <h2>Hello, <?php echo htmlspecialchars($name); ?>!</h2>
     <h3>Your BMI is: <?php echo number_format($bmi, 2); ?></h3>
 
@@ -23,4 +30,14 @@
     <a href="main.php">Calculate again</a>
     </center>
 </body>
+<script>
+    
+    const btn = document.getElementById("mode");
+    const frame = document.body;
+
+    btn.addEventListener('click',()=>{
+        console.log("button clicked");
+        frame.classList.toggle("night-mode");
+    });
+</script>
 </html>
