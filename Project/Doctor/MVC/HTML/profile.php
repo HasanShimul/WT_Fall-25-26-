de8ccfa34977be63b1f947706127327ca027f198
=ShimulHasan <=ss.shimulhasan00@gmail.com>
2026-01-05 12:18:00 +0600

    system dark dark mode added

--- Project/Doctor/MVC/HTML/profile.php
+++ Project/Doctor/MVC/HTML/profile.php
@@ -12,7 +12,21 @@
 <body>
     <main>
         <div>
-            <h1>Personal informatin</h1>
+            <div><h1>Personal informatin</h1></div>
+            <div>
+                <form action="">
+                    <table>
+                        <tr>
+                            <td><label for="drname"> </label></td>
+                            <!-- <td><input type="text"></td> -->
+                        </tr>
+                        <tr>
+                            <td><label for="drtype"> </label></td>
+                            <!-- <td><input type="text" name="" id=""></td> -->
+                        </tr>
+                    </table>
+                </form>
+            </div>
         </div>
     </main>
 
